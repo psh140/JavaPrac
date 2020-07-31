9c11c3dc6222baef9bfdfb843b5b53081f352fc7
Hugh <psh140140@gmail.com>
2020-07-31 17:11:58 +0900

    Day 3

---
+++ Day3/src/Circle.java
@@ -0,0 +1,33 @@
+//
+//public class Circle {
+//	int radius;
+//	String name;
+//
+//	public Circle() {
+//		radius = 1;
+//		name = "";
+//	}
+//
+//	public Circle(int radius, String name) {
+//		this.radius = radius;
+//		this.name = name;
+//	}
+//
+//	public double getArea() {
+//		return 3.14 * radius * radius;
+//	}
+//
+//	public static void main(String[] args) {
+//		Circle pizza;
+//		pizza = new Circle(10, "JavaPizza");
+//
+//		double area = pizza.getArea();
+//		System.out.println(pizza.name + "'s extent is " + area);
+//
+//		Circle donut = new Circle();
+//		donut.name = "JavaDonut";
+//		area = donut.getArea();
+//		System.out.println(donut.name + "'s extent is " + area);
+//	}
+//
+//}
