2370d0416995dc04622a33f4e3c73be2f171e3ef
Hugh <psh140140@gmail.com>
2020-08-17 11:59:34 +0900

    Fixed SwingPractice

---
+++ SwingPractice/src/Management/Data.java
@@ -0,0 +1,35 @@
+package Management;
+
+public class Data {
+	private String name;
+	private int korea;
+	private int english;
+	private int math;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKorea() {
+		return korea;
+	}
+	public void setKorea(int korea) {
+		this.korea = korea;
+	}
+	public int getEnglish() {
+		return english;
+	}
+	public void setEnglish(int english) {
+		this.english = english;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	
+	
+}
